10bbcd6d0ea644546cca15bd506913ce05f7f6fb
Abhishek Singh <abhisheksingh4115@gmail.com>
2025-05-09 12:55:53 +0530

    dist file updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    BACKEND_URL : "https://matchably-backend.onrender.com",
+    BACKEND_URL : "http://51.21.213.116/api",
     // BACKEND_URL : "http://localhost:2340",
 }
 //https://matchably-backend.onrender.com
