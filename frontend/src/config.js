fe7e2cf088b96ceb0adf9d0aa0125d391fe2fe1a
Abhishek Singh <info@backendcodersindia.com>
2025-05-11 23:30:33 -0700

    new folder update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-    BACKEND_URL : "https://matchably.kr/api",
+    // BACKEND_URL : "https://matchably-backend.onrender.com",
+    BACKEND_URL : "http://localhost:2340/api",
 }
-
+//https://matchably-backend.onrender.com
+//http://localhost:2340
 export default config;
